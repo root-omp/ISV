669594a44367c9fca3af6dc8cc6efdd430ce1b2f
root_omp <petr.sviridenkov@gmail.com>
2022-09-21 01:24:02 +0300

    v0.0.0.1

--- ISV/main.py
+++ ISV/main.py
@@ -1,15 +1,12 @@
 from config import Config
-import nextcord
-import random
-import json
-import requests
+from nextcord import Embed
 from nextcord.ext import commands
 import os
 
 
 client = commands.Bot(command_prefix=Config.prefix,
                       owner_ids=Config.ids,
-                      intents=nextcord.Intents.default(),
+                      intents=Config.intents,
                       help_command=None)
 
 client.remove_command("help")
@@ -19,14 +16,14 @@ client.remove_command("help")
 @commands.is_owner()
 async def _load(ctx, cog: str = None):
     if cog is None:
-        emd = nextcord.Embed(title='Ошибка', description='Вы не указали ког')
+        emd = Embed(title='Ошибка', description='Вы не указали ког')
     else:
         try:
             client.load_extension(f'cogs.{cog}')
-            emb = nextcord.Embed(
+            emd = Embed(
                 title='Ког', description=f'Ког {cog} был успешно загружен', color=0xFF7F50)
         except Exception as e:
-            return await ctx.reply(embed=nextcord.Embed(title='Ошибка', description=f'Не найден ког {cog}', color=0xff0000))
+            return await ctx.reply(embed=Embed(title='Ошибка', description=f'Не найден ког {cog}', color=0xff0000))
     return await ctx.reply(embed=emd)
 
 
@@ -34,14 +31,14 @@ async def _load(ctx, cog: str = None):
 @commands.is_owner()
 async def _unload(ctx, cog: str = None):
     if cog is None:
-        emd = nextcord.Embed(title='Ошибка', description='Вы не указали ког')
+        emd = Embed(title='Ошибка', description='Вы не указали ког')
     else:
         try:
             client.unload_extension(f'cogs.{cog}')
-            emd = nextcord.Embed(
+            emd = Embed(
                 title='Ког', description=f'Ког {cog} был успешно отгружен', color=0xFF7F50)
         except Exception as e:
-            return await ctx.reply(embed=nextcord.Embed(title='Ошибка', description=f'Не найден ког {cog}', color=0xff0000))
+            return await ctx.reply(embed=Embed(title='Ошибка', description=f'Не найден ког {cog}', color=0xff0000))
     return await ctx.reply(embed=emd)
 
 
@@ -49,15 +46,15 @@ async def _unload(ctx, cog: str = None):
 @commands.is_owner()
 async def _reload(ctx, cog: str = None):
     if cog is None:
-        emd = nextcord.Embed(title='Ошибка', description='Вы не указали ког')
+        emd = Embed(title='Ошибка', description='Вы не указали ког')
     else:
         try:
             client.unload_extension(f'cogs.{cog}')
             client.load_extension(f'cogs.{cog}')
-            emd = nextcord.Embed(
+            emd = Embed(
                 title='Ког', description=f'Ког {cog} был успешно перезагружен', color=0xFF7F50)
         except Exception as e:
-            return await ctx.reply(embed=nextcord.Embed(title='Ошибка', description=f'Не найден ког {cog}', color=0xff0000))
+            return await ctx.reply(embed=Embed(title='Ошибка', description=f'Не найден ког {cog}', color=0xff0000))
     return await ctx.reply(embed=emd)
 
 # + load all cogs
@@ -69,4 +66,4 @@ for x in os.listdir("./cogs"):
 
 print('Коги загружены!')
 
-client.run(Config.token)
+client.run(Config.test_token)
